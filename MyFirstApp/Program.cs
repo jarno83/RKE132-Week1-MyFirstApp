1796186b224b334718c4e9b6b387dec8d57c0000
Jarno Orasmäe <jarnoorasmae@tktk.ee>
2023-09-06 20:33:15 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// This is a comment to my code
+
+Console.WriteLine("Enter your name:");
+
+//String - sõne
+string userName = Console.ReadLine();
+Console.WriteLine("Hello, " + userName + "!");
+
+//string insterpolation
+Console.WriteLine($"Hello, {userName}!");
